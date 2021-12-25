9535317b4f717223420ccf2135c24e8a010ece5b
Henokaa <Henoktadele9@gmail.com>
2021-12-25 16:09:37 +0300

    company_logo

--- Hacker rank.py
+++ Hacker rank.py
@@ -60,3 +60,21 @@ for t in range(int(input())):
         print("Yes")
     else:
         print("No")
+
+#company logo : collection
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+from collections import Counter
+
+class OrderedCounter(Counter):
+    pass
+
+if __name__ == '__main__':
+    [print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
+
