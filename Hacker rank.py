c304de794c09d52e6432f70b74af30c1ff22603d
Henokaa <Henoktadele9@gmail.com>
2021-12-25 16:03:32 +0300

    pilling

--- Hacker rank.py
+++ Hacker rank.py
@@ -48,3 +48,15 @@ a = map(int, input().split())
 b = map(int, input().split())
 
 print(*product(a, b))
+
+#pilling
+for t in range(int(input())):
+    input()
+    lst = [int(i) for i in input().split()]
+    min_list = lst.index(min(lst))
+    left = lst[:min_list]
+    right = lst[min_list+1:]
+    if left == sorted(left,reverse=True) and right == sorted(right):
+        print("Yes")
+    else:
+        print("No")
