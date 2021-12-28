1d209598253e44ea77be8ae5bda7a3c07e61880a
Henokaa <Henoktadele9@gmail.com>
2021-12-28 16:53:39 +0300

    company_logo

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-x = input() 
-print(x)         #' insert 0 5     '
-print(x.strip())  #'insert 0 5'
-print(x.strip().split())  #['insert', '0', '5'
-print(x.split())
+for num in range(5):
+    print(num)
