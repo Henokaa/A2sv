b72010019963f5f0a599f580ba32ccf90c98ed9f
Henokaa <Henoktadele9@gmail.com>
2021-12-28 18:25:26 +0300

    company_logo

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-for num in range(5):
-    print(num)
+class Student ():
+    _age = 0
+    STUDENT_ID 
+    def __init__(self, length):
+        self.length = length
+        self.STUDENT_ID = 459
+
+    def GradeReport(self, name, age):
+        self.age = age
+        self.name = name
